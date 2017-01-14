56600d3199a11c3997698a10711b89b6da097325
Ahmet Atalay <ahmet@opsgenie.com>
2017-01-14 21:26:30 +0300

    first commit

---
+++ src/main/java/com/example/ContinuousIntegrationAndContinuousDeliveryAppApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class ContinuousIntegrationAndContinuousDeliveryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuousIntegrationAndContinuousDeliveryAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by ahmetatalay on 14/01/17.
+ */
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello!";
+    }
+
+}
