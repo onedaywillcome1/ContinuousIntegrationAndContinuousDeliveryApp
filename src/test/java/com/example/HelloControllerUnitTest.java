1bdc1b097c6b43278a7234890f262bfbe5944748
Ahmet Atalay <ahmet@opsgenie.com>
2017-01-15 18:00:09 +0300

    commit

--- src/test/java/com/example/HelloControllerUnitTest.java
+++ src/test/java/com/example/HelloControllerUnitTest.java
@@ -32,7 +32,7 @@ public class HelloControllerUnitTest {
     public void getHello() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello World!")));
+                .andExpect(content().string(equalTo("Hello Ahmet!")));
     }
 
 }
